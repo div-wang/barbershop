4eae2b6f726dc8d776b6b7c5a927b56326ade087
div-wang <gh110827@gmail.com>
2016-03-08 19:04:04 +0800

    add 分页查询

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -5,30 +5,16 @@
 		init: function(){
 			var that = this
 			$('#search').on('click', function(event) {
-				var data = {},
-					name = $('#name').val(),
-				    phone = $('#phone').val(),
-				    cardType = $('#cardType').val();
-				if(name!=''){
-					data.name = name
-				}
-				if(phone!=''){
-					data.phone = phone
-				}
-				if(cardType!=''){
-					data.cardType = cardType
-				}
-				that.getData(data)
+				that.getData(0)
 			});
-			$('#search').click();
-
+			that.getData(0)
 		},
 		showList:function(res){
 			var html = $('<table class="table table-hover table-bordered"><thead><tr><th class="">会员卡号</th><th class="">会员卡类型</th><th class="">时间</th><th class="">已用次数</th></tr></thead><tbody></tbody></table>')
 			$.each(res.data,function(index, el) {
 				html.find('tbody').append('<tr><td>'+el.cardNo+'</td><td>'+el.type+'</td><td>'+new Date(el.date).toString()+'</td><td>'+el.num+'</td></tr>')
 			});
-			$('.main-table').append(html)
+			$('.main-table').html(html)
 		},
 		showPage: function(res){
 			var that = this
@@ -40,12 +26,27 @@
 		        pageSize:10,
 		        total: res.total,
 		        onPageChange: function(oldPaage, newPage){
-		            that.loadData(newPage)
+		            that.getData(newPage)
 		        }
 		    })
 		},
-		getData: function(data){
-			var that = this
+		getData: function(page){
+			var that = this,
+				data = {},
+				name = $('#name').val(),
+			    phone = $('#phone').val(),
+			    cardType = $('#cardType').val();
+			if(name!=''){
+				data.name = name
+			}
+			if(phone!=''){
+				data.phone = phone
+			}
+			if(cardType!=''){
+				data.cardType = cardType
+			}
+			data.pageNum = page;
+			data.pageSize = 10;
 			$.ajax({
 				url: '/queryCardAll',
 				type: 'GET',
